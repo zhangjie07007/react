d47d8b055ce93541e7a4759a6a75a1754ad0bfaf
Dich丶 <1591391677@qq.com>
2019-08-21 21:42:04 +0800

    删除了一些没必要的代码

--- src/Views/Article/index.js
+++ src/Views/Article/index.js
@@ -43,22 +43,6 @@ class Article extends Component {
         selectedRowKeys: [], // Check here to configure the default column
         isload:false,
         isPagenation:false,
-        list:[
-            {
-                postId: 1,
-                id: 1,
-                name: "id labore ex et quam laborum",
-                email: "Eliseo@gardner.biz",
-                body: "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
-            },
-            {
-                "postId": 1,
-                "id": 2,
-                "name": "quo vero reiciendis velit similique earum",
-                "email": "Jayne_Kuhic@sydney.com",
-                "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
-            }
-        ]
     };
     componentDidMount(){
         // setTimeout(()=>{
@@ -140,8 +124,6 @@ class Article extends Component {
                     pagination = {{
                         pageSize:6
                     }}
-                   
-                
                  />;
 
             </Card>
