3ef9ccfe490131bf8a035aef756015ef84aa3caf
Dich丶 <1591391677@qq.com>
2019-08-23 11:32:21 +0800

    uuu

--- src/Views/Article/Edit.js
+++ src/Views/Article/Edit.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react'
-import {Card} from 'antd'
+import { Form, Icon, Input, Button, Card } from 'antd';
 import './edit.less'
-export default class Edit extends Component {
+@Form.create()
+class Edit extends Component {
     render() {
         console.log(this.props)
+
+        const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+        // Only show error after a field is touched.
+        const usernameError = isFieldTouched('username') && getFieldError('username');
+        const passwordError = isFieldTouched('password') && getFieldError('password');
+
         return (
             <div className='edit-box' style={{ background: '#ECECEC', padding: '30px' }}>
-                <Card 
-                title='编辑文章' 
-                bordered={false}
+                <Card
+                    title='编辑文章'
+                    bordered={false}
                 >
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
+                    <Form layout="inline" onSubmit={this.handleSubmit}>
+                        <Form.Item >
+                            {getFieldDecorator('username', {
+                                rules: [
+                                    { 
+                                        required: true, 
+                                        message: 'Please input your username!' 
+                                    },{
+                                        min:6,
+                                        message:'最少六位'
+                                    }
+                                ],
+                            })(
+                                <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
+                                />,
+                            )}
+                        </Form.Item>
+                    </Form>
                 </Card>
             </div>
         )
     }
 }
+export default Edit
